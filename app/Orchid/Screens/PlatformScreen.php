8c3c9361673590ce2a811555311e020b828ead44
Agung Gunawan <agunggunawan.github@gmail.com>
2022-02-22 10:26:43 +0700

    update PlatformScreen

--- app/Orchid/Screens/PlatformScreen.php
+++ app/Orchid/Screens/PlatformScreen.php
@@ -10,63 +10,30 @@ use Orchid\Support\Facades\Layout;
 
 class PlatformScreen extends Screen
 {
-    /**
-     * Query data.
-     *
-     * @return array
-     */
     public function query(): iterable
     {
         return [];
     }
 
-    /**
-     * Display header name.
-     *
-     * @return string|null
-     */
     public function name(): ?string
     {
         return 'Get Started';
     }
 
-    /**
-     * Display header description.
-     *
-     * @return string|null
-     */
     public function description(): ?string
     {
         return 'Welcome to your Orchid application.';
     }
 
-    /**
-     * Button commands.
-     *
-     * @return \Orchid\Screen\Action[]
-     */
     public function commandBar(): iterable
     {
         return [
-            Link::make('Website')
-                ->href('http://orchid.software')
+            Link::make('Numerology')
+                ->href('/dashboard')
                 ->icon('globe-alt'),
-
-            Link::make('Documentation')
-                ->href('https://orchid.software/en/docs')
-                ->icon('docs'),
-
-            Link::make('GitHub')
-                ->href('https://github.com/orchidsoftware/platform')
-                ->icon('social-github'),
         ];
     }
 
-    /**
-     * Views.
-     *
-     * @return \Orchid\Screen\Layout[]
-     */
     public function layout(): iterable
     {
         return [
