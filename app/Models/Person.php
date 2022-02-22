1fd88401d4e5d95b7f76bbcd3224c61c313a4f4e
Agung Gunawan <agunggunawan.github@gmail.com>
2022-02-22 10:18:34 +0700

    create Person orchid resource

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -5,14 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Orchid\Filters\Filterable;
+use Orchid\Screen\AsSource;
 
 class Person extends Model
 {
-    use HasFactory;
+    use AsSource, HasFactory, Filterable;
 
     protected $fillable = [
         'name',
         'birth_date',
+    ];
+
+    protected $casts = [
+        'birth_date' => 'date',
     ];
 
     public function user(): BelongsTo
