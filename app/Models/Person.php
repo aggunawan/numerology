dd5309572c80e3d5491afa5a19b8501ebc38fd8d
Agung Gunawan <agunggunawan.github@gmail.com>
2022-02-22 09:48:28 +0700

    create Person table

---
+++ app/Models/Person.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Person extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'birth_date',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
