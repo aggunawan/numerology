209e1dea4eb5beab54ce9f8996e4fa8cab6e6f15
Agung Gunawan <agunggunawan.github@gmail.com>
2022-02-25 16:26:54 +0700

    Date of Birth model three section

--- resources/views/livewire/date-of-birth.blade.php
+++ resources/views/livewire/date-of-birth.blade.php
@@ -1,19 +1,6 @@
 <div class="relative bg-white rounded-lg shadow">
     <div class="p-6 grid grid-cols-12 gap-2">
-        <div class="flex flex-col gap-2 col-span-5">
-            @if($this->list)
-                @foreach($this->list->content as $i => $content)
-                    <button
-                        wire:click="removeList({{ $i }})"
-                        class="flex flex-col items-center p-3 text-base font-bold text-gray-900 bg-gray-200 rounded-lg hover:bg-gray-100 group hover:shadow">
-                        <strong>{{ $content['name'] }}</strong>
-                        <small>{{ now()->parse($content['date'])->format('d F Y') }}</small>
-                    </button>
-                @endforeach
-            @endif
-        </div>
-
-        <div class="flex flex-col gap-2 col-span-7">
+        <div class="flex flex-col col-span-3 gap-2">
             @if($this->has_list)
                 <div class="flex flex-col gap-2">
                     <label for="people" class="block text-sm font-medium text-gray-900">People Database</label>
@@ -21,7 +8,7 @@
                         wire:model="selectedPerson"
                         id="people"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full">
-                        <option value="">Select from database</option>
+                        <option value="">Select</option>
                         @foreach($people as $id => $person)
                             <option value="{{ $id }}">{{ $person }}</option>
                         @endforeach
@@ -33,7 +20,9 @@
                         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Save</button>
                 </div>
             @endif
+        </div>
 
+        <div class="flex flex-col col-span-5 gap-2">
             <div class="flex flex-col gap-2">
                 <label for="name" class="block text-sm font-medium text-gray-900">Name</label>
                 <input
@@ -42,8 +31,8 @@
                     type="text">
             </div>
 
-            <div class="w-full grid grid-cols-4 gap-2">
-                <div class="flex flex-col gap-2">
+            <div class="w-full grid grid-cols-3 gap-2">
+                <div class="w-full flex flex-col gap-2">
                     <label class="block text-sm font-medium text-gray-900">Date</label>
                     <select
                         wire:model="selectedDate"
@@ -53,7 +42,7 @@
                         @endforeach
                     </select>
                 </div>
-                <div class="w-full col-span-2 flex flex-col gap-2">
+                <div class="w-full flex flex-col gap-2">
                     <label class="block text-sm font-medium text-gray-900">Month</label>
                     <select
                         wire:model="selectedMonth"
@@ -78,6 +67,20 @@
                     wire:click="storeList"
                     class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Save</button>
             </div>
+        </div>
+
+        <div class="flex flex-col col-span-4 gap-2">
+            <p class="mb-0">Date List</p>
+            @if($this->list)
+                @foreach($this->list->content as $i => $content)
+                    <button
+                        wire:click="removeList({{ $i }})"
+                        class="flex flex-col items-center p-3 text-base font-bold text-gray-900 bg-gray-200 rounded-lg hover:bg-gray-100 group hover:shadow">
+                        <strong>{{ $content['name'] }}</strong>
+                        <small>{{ now()->parse($content['date'])->format('d F Y') }}</small>
+                    </button>
+                @endforeach
+            @endif
         </div>
     </div>
 
