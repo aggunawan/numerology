8a6b449d0e7fd74879a4846d6bda3bfd9cf99ba6
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-01 13:17:54 +0700

    add Trash icon to delete DOB list

--- resources/views/livewire/date-of-birth.blade.php
+++ resources/views/livewire/date-of-birth.blade.php
@@ -77,12 +77,20 @@
             <p class="mb-0">DOB List</p>
             @if($this->list)
                 @foreach($this->list->content as $i => $content)
-                    <button
-                        wire:click="removeList({{ $i }})"
-                        class="flex flex-col items-center p-3 text-base font-bold text-gray-900 bg-gray-200 rounded-lg hover:bg-gray-100 group hover:shadow">
+                    <div
+                        class="flex flex-col p-3 text-base font-bold text-gray-900 bg-gray-200 rounded-lg hover:bg-gray-100 group hover:shadow">
                         <strong>{{ $content['name'] }}</strong>
-                        <small>{{ now()->parse($content['date'])->format('d F Y') }}</small>
-                    </button>
+                        <div class="w-full flex flex-row justify-between">
+                            <small>{{ now()->parse($content['date'])->format('d F Y') }}</small>
+                            <button
+                                wire:click="removeList({{ $i }})"
+                                class="hover:text-red-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
                 @endforeach
             @endif
         </div>
