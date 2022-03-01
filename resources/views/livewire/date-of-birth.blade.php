6dba23243763884ae559e9852ff7775e80785ac6
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-01 11:49:40 +0700

    close button at DOB modal

--- resources/views/livewire/date-of-birth.blade.php
+++ resources/views/livewire/date-of-birth.blade.php
@@ -88,7 +88,13 @@
         </div>
     </div>
 
-    <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+    <div class="grid grid-cols-2 items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
+        <button
+            data-modal-toggle="dateOfBirthModal"
+            type="button"
+            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600">
+            Close
+        </button>
         <a
             type="button"
             class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center"
