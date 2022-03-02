91745ee4a7413050520a4a5e487f431a45d40bf5
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-02 11:13:51 +0700

    update UI card at dashboard index

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
                 id="profile"
                 role="tabpanel"
                 aria-labelledby="profile-tab">
-                <div class="grid grid-cols-6 gap-2">
+                <div class="lg:grid lg:grid-cols-6 gap-2 hidden">
                     <div class="col-start-2">
                         @include('numerology.dark-category', ['numerology' => $numerology->getDayMaster(), 'palaces' => $palaces, 'year' => $highlightedYear])
                     </div>
@@ -16,7 +16,7 @@
                     @include('numerology.category', ['numerology' => $numerology->getEducation(), 'palaces' => $palaces, 'year' => $highlightedYear])
                     @include('numerology.category', ['numerology' => $numerology->getCulture(), 'palaces' => $palaces, 'year' => $highlightedYear])
                 </div>
-                <div class="grid grid-cols-6 gap-2">
+                <div class="lg:grid lg:grid-cols-6 gap-2 hidden">
                     @include('numerology.category', ['numerology' => $numerology->getTalent(), 'palaces' => $palaces, 'year' => $highlightedYear])
                     @include('numerology.category', ['numerology' => $numerology->getPartner(), 'palaces' => $palaces, 'year' => $highlightedYear])
                     @include('numerology.category', ['numerology' => $numerology->getBelief(), 'palaces' => $palaces, 'year' => $highlightedYear])
@@ -24,7 +24,7 @@
                     @include('numerology.category', ['numerology' => $numerology->getAmbition(), 'palaces' => $palaces, 'year' => $highlightedYear])
                     @include('numerology.category', ['numerology' => $numerology->getBusiness(), 'palaces' => $palaces, 'year' => $highlightedYear])
                 </div>
-                <div class="grid grid-cols-3 gap-2">
+                <div class="lg:grid lg:grid-cols-3 gap-2 hidden">
                     <div class="grid grid-cols-2 gap-2">
                         @include('numerology.category', ['numerology' => $numerology->getSpiritual(), 'palaces' => $palaces, 'year' => $highlightedYear])
                         @include('numerology.category', ['numerology' => $numerology->getEmotional(), 'palaces' => $palaces, 'year' => $highlightedYear])
@@ -46,6 +46,36 @@
                         <div class="col-span-2">
                             @include('numerology.category-with-hidden', ['numerology' => $numerology->getHealth(), 'palaces' => $palaces, 'swap' => true, 'year' => $highlightedYear])
                         </div>
+                    </div>
+                </div>
+                <div class="flex lg:hidden justify-center">
+                    <div class="grid grid-cols-2 gap-2 grow max-w-md">
+                        @include('numerology.dark-category', ['numerology' => $numerology->getDayMaster(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getCulture(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getMindset(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getEducation(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getBelief(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getCareer(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getPartner(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getAmbition(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getTalent(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getBusiness(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getSpiritual(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getIntellectual(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getEmotional(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getSocial(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getRelationship(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getFinancial(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getSon(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category', ['numerology' => $numerology->getDaughter(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                    </div>
+                </div>
+                <div class="flex lg:hidden justify-center">
+                    <div class="grid gap-2 grow max-w-md">
+                        @include('numerology.category-with-hidden', ['numerology' => $numerology->getCharacter(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category-with-hidden', ['numerology' => $numerology->getHealth(), 'palaces' => $palaces, 'swap' => true, 'year' => $highlightedYear])
+                        @include('numerology.category-bigger', ['numerology' => $numerology->getPhysical(), 'palaces' => $palaces, 'year' => $highlightedYear])
+                        @include('numerology.category-without-year', ['numerology' => $numerology->getGoal(), 'palaces' => $palaces])
                     </div>
                 </div>
             </div>
