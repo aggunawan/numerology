6d31c9905330f4f013f3bc19424d10d060b183bf
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-02 10:46:34 +0700

    update UI at login and register page

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -3,7 +3,9 @@
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="p-4 sm:p-0">
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            {{ $slot }}
+        </div>
     </div>
 </div>
