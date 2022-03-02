1a686bf1d4c72270b31c82620a53a177ee742c16
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-02 14:53:36 +0700

    scrollable Monthly Insight

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -85,8 +85,8 @@
                 role="tabpanel"
                 aria-labelledby="dashboard-tab">
                 <div class="p-4 w-full sm:max-w-md md:max-w-full mx-auto bg-white rounded-lg border shadow-md">
-                    <div class="flex justify-between">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Monthly Insights</h5>
+                    <div class="flex flex-col md:flex-row justify-between">
+                        <h5 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Monthly Insights</h5>
                         <form action="{{ route('dashboard.index') }}" method="GET">
                             <div class="max-w-sm grid grid-cols-3 gap-4">
                                 <input type="hidden" name="tab" value="year">
@@ -96,22 +96,22 @@
                                         type="number"
                                         value="{{ $currentYear }}"
                                         placeholder="Year"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-4 p-2.5 datepicker-input">
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-4 p-2 datepicker-input">
                                 </div>
                                 <button
                                     type="submit"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center">Submit</button>
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2 text-center">Submit</button>
                             </div>
                         </form>
                     </div>
 
                     <div class="flex flex-col">
                         <div class="inline-block py-2 min-w-full">
-                            <div class="overflow-hidden shadow-md sm:rounded-lg">
+                            <div class="overflow-x-auto shadow-md rounded-lg">
                                 <table class="min-w-full">
                                     <thead class="bg-gray-200 border-gray-200 font-bold">
                                     <tr>
-                                        <th scope="col" class="py-3 px-6 text-sm font-bold tracking-wider text-left text-gray-700 uppercase text-center">
+                                        <th scope="col" class="py-2 text-sm font-bolder tracking-wider text-left text-gray-700 uppercase text-center">
                                             Year
                                         </th>
                                         @foreach(range(1, 31) as $date)
