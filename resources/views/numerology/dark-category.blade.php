9cdd1a3d28a4dfb593cdc38e7d52e62ae53144a2
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-02 13:15:12 +0700

    make active dashboard card darker

--- resources/views/numerology/dark-category.blade.php
+++ resources/views/numerology/dark-category.blade.php
@@ -1,4 +1,4 @@
-<div class="p-2 w-full sm:max-w-md md:max-w-full mx-auto bg-gray-800 rounded-lg border shadow-md text-white {{ ($year ?? 0 ) == $numerology->getYear() ? 'border-lime-400 border-8' : null }}">
+<div class="p-2 w-full sm:max-w-md md:max-w-full mx-auto bg-gray-800 rounded-lg border shadow-md text-white {{ ($year ?? 0 ) == $numerology->getYear() ? 'border-green-400 border-8' : null }}">
     <div class="flex justify-between">
         <p class="text-xs font-extrabold uppercase px-2 mb-0">
             {{ $numerology->getName() }}
