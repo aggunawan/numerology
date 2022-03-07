bc3766d694911c4d6dd61dbdb7a0eaa27485f34c
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-07 15:26:04 +0700

    Implement dynamic palace description to another card

--- resources/views/numerology/category-bigger.blade.php
+++ resources/views/numerology/category-bigger.blade.php
@@ -9,10 +9,14 @@
     </div>
 
     <ul class="w-full grid gap-1">
-        @foreach($numerology->getTraitCodes() as $trait)
+        @foreach($numerology->getTraitCodes() as $i => $trait)
+            @php
+                $palace = isset($palaces[$trait]) ? $palaces[$trait][0] : $numerology->getTraits()[$trait];
+            @endphp
             <li>
                 @include('numerology.category.trait', [
-                    'trait' => isset($palaces[$trait]) ? $palaces[$trait][0] : $numerology->getTraits()[$trait],
+                    'trait' => $palace,
+                    'modalName' => \Illuminate\Support\Str::camel($numerology->getName() . $palace . ($i + 1)),
                     'color' => isset($palaces[$trait]) ? $palaces[$trait][2] : null,
                     'backgroundColor' => isset($palaces[$trait]) ? $palaces[$trait][1] : null,
                     'buttonClass' => 'p-1',
--- resources/views/numerology/category/hidden-trait.blade.php
+++ resources/views/numerology/category/hidden-trait.blade.php
@@ -1,5 +1,6 @@
 <button
     type="button"
+    data-modal-toggle="{{ $modalName ?? null }}"
     class="w-full bg-transparent text-center flex items-center text-base font-bold rounded-lg text-red-500">
     <small class="flex-1 whitespace-nowrap">{{ $trait }}</small>
 </button>
