bd6f88626be1afab417be6278b85fee7dd097f96
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-08 10:16:38 +0700

    create remark card UI

---
+++ resources/views/numerology/remark.blade.php
@@ -0,0 +1,21 @@
+<div class="p-2 w-full sm:max-w-md md:max-w-full mx-auto bg-white rounded-lg shadow-md">
+    <div class="flex flex-col px-2">
+        <p class="text-xl mb-4">Foo bar</p>
+
+        <div class="grid gap-4">
+            <div class="flex flex-col">
+                <p class="text-base">
+                    Lorem ipsum dolor
+                </p>
+                <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, pariatur, perspiciatis. Alias aliquid beatae, culpa cum dignissimos, dolores excepturi facilis illum ipsam iste labore nam, nostrum perferendis quia quisquam similique?</p>
+            </div>
+
+            <div class="flex flex-col">
+                <p class="text-base">
+                    Lorem ipsum dolor
+                </p>
+                <p class="text-gray-600">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, pariatur, perspiciatis. Alias aliquid beatae, culpa cum dignissimos, dolores excepturi facilis illum ipsam iste labore nam, nostrum perferendis quia quisquam similique?</p>
+            </div>
+        </div>
+    </div>
+</div>
