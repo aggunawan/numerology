3839b644f796f9a9a8f28e37240f0764a200452a
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-09 14:18:32 +0700

    process PalaceDescription import at background

--- app/Listeners/ProcessPalaceDescriptionExcel.php
+++ app/Listeners/ProcessPalaceDescriptionExcel.php
@@ -4,9 +4,10 @@ namespace App\Listeners;
 
 use App\Events\PalaceDescriptionExcelImported;
 use App\Imports\PalaceDescriptionImport;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Maatwebsite\Excel\Facades\Excel;
 
-class ProcessPalaceDescriptionExcel
+class ProcessPalaceDescriptionExcel implements ShouldQueue
 {
     public function handle(PalaceDescriptionExcelImported $event)
     {
