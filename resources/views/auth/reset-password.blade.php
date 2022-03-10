baf0da5ff6e40dd1520d59eccd891d435d166c65
Agung Gunawan <agunggunawan.github@gmail.com>
2022-03-10 13:19:41 +0700

    logo at forgot-password and reset-password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,9 +1,7 @@
 <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
+            <img src="{{ asset('img/logo.jpeg') }}" class="w-60" alt="Logo">
         </x-slot>
 
         <!-- Validation Errors -->
